90b54379446408cd68fd15a73897a373da629239
Taylor Baldwin <taylorbaldwin@gmail.com>
2017-02-21 14:48:22 -0500

    using yo-yo with some refactoring

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import palettes from './data/color-palettes.json'
 import Color from 'color'
 import {GUI} from 'dat-gui'
+import yo from 'yo-yo'
 
 const thumbnailDir = 'data/thumbs'
 
@@ -9,27 +10,31 @@ const container = document.querySelector('.container')
 const films = Object.keys(palettes)
 
 const settings = {
-  canvasSize: 450,
-  showColorConnections: false,
-  colorThreshold: 0.02,
-  screenshotCircleSize: 2,
+  canvasSize: 500,
+  showColorConnections: true,
+  colorThreshold: 0,
+  screenshotCircleSize: 0,
   colorCircleMaxSize: 15,
   magnitudeDimension: 'lightness',
   circleSizeDimensionNegative: false,
   circleSizeDimension: 'saturation',
-  magnitudeDimensionNegative: true
+  magnitudeDimensionNegative: true,
+  ticksAlpha: 0.04,
+  showColorWheel: false
 }
 
 const gui = new GUI()
-gui.add(settings, 'canvasSize', 50, 1000).step(10).onChange(renderPage)
-gui.add(settings, 'colorThreshold', 0, 1.0).step(0.01).onChange(renderPage)
-gui.add(settings, 'screenshotCircleSize', 0, 10).step(1).onChange(renderPage)
-gui.add(settings, 'colorCircleMaxSize', 1, 40).step(1).onChange(renderPage)
-gui.add(settings, 'circleSizeDimension', ['lightness', 'saturation']).onChange(renderPage)
-gui.add(settings, 'circleSizeDimensionNegative').onChange(renderPage)
-gui.add(settings, 'magnitudeDimension', ['lightness', 'saturation']).onChange(renderPage)
-gui.add(settings, 'magnitudeDimensionNegative').onChange(renderPage)
+// gui.add(settings, 'canvasSize', 50, 1000).step(10).onChange(renderPage)
+gui.add(settings, 'colorThreshold', 0, 100).step(1).onChange(renderPage)
+// gui.add(settings, 'screenshotCircleSize', 0, 10).step(1).onChange(renderPage)
+// gui.add(settings, 'colorCircleMaxSize', 1, 40).step(1).onChange(renderPage)
+// gui.add(settings, 'circleSizeDimension', ['lightness', 'saturation']).onChange(renderPage)
+// gui.add(settings, 'circleSizeDimensionNegative').onChange(renderPage)
+// gui.add(settings, 'magnitudeDimension', ['lightness', 'saturation']).onChange(renderPage)
+// gui.add(settings, 'magnitudeDimensionNegative').onChange(renderPage)
 gui.add(settings, 'showColorConnections').onChange(renderPage)
+// gui.add(settings, 'ticksAlpha', 0, 0.2).step(0.01).onChange(renderPage)
+gui.add(settings, 'showColorWheel').onChange(renderPage)
 
 renderPage()
 
@@ -37,68 +42,38 @@ function renderPage () {
   const { canvasSize } = settings
   container.innerHTML = ''
   const filmEls = films.map(film => {
-    const filmEl = document.createElement('div')
-    filmEl.style.width = `${canvasSize}px`
-    filmEl.classList.add('film')
-    filmEl.innerHTML = `<h2>${film}</h2>`
-
     const screenshots = palettes[film]
     const padding = canvasSize * 0.05 | 0
-    const canvas = createCanvas(canvasSize, canvasSize, padding)
-    filmEl.appendChild(canvas)
+    const canvas = yo`
+      <canvas
+        height=${canvasSize}
+        width=${canvasSize}
+        style="width: ${canvasSize}px; height: ${canvasSize}px;" />
+    `
     const ctx = canvas.getContext('2d')
-
     renderViz(ctx, screenshots, padding)
 
-    const screenshotsContainer = renderScreenshots(screenshots)
-    filmEl.appendChild(screenshotsContainer)
-    return filmEl
+    return yo`
+      <div class="film" style="width: ${canvasSize}px">
+        <h2>${film}</h2>
+        ${canvas}
+        <div class="screenshots">
+          ${screenshots.map(screenshot => yo`
+            <div>
+              <ul>
+                ${getColorsFromScreenshot(screenshot).map(color =>
+                  yo`<li style="background-color: rgb(${color.join(',')})" />`
+                )}
+              </ul>
+              <img src="${thumbnailDir}/${screenshot.src}" />
+            </div>
+          `)}
+        </div>
+      </div>
+    `
   })
 
   filmEls.forEach(filmEl => container.appendChild(filmEl))
-}
-
-function renderScreenshots (screenshots) {
-  const screenshotsContainer = document.createElement('div')
-  screenshotsContainer.classList.add('screenshots')
-  screenshots.forEach(screenshot => {
-    const colors = getColorsFromScreenshot(screenshot)
-    const screenshotDiv = document.createElement('div')
-    const img = document.createElement('img')
-    img.src = `${thumbnailDir}/${screenshot.src}`
-    const swatch = renderSwatch(colors)
-    screenshotDiv.appendChild(swatch)
-    screenshotDiv.appendChild(img)
-    screenshotsContainer.appendChild(screenshotDiv)
-  })
-  return screenshotsContainer
-}
-
-function renderSwatch (colors) {
-  const ul = document.createElement('ul')
-  colors.forEach(color => {
-    const li = document.createElement('li')
-    li.style.backgroundColor = `rgb(${color.join(',')})`
-    ul.appendChild(li)
-  })
-  return ul
-}
-
-function renderSwatches (screenshots) {
-  const swatchesContainer = document.createElement('div')
-  swatchesContainer.classList.add('swatches')
-  const swatches = screenshots.map(screenshot => {
-    const colors = getColorsFromScreenshot(screenshot)
-    const ul = document.createElement('ul')
-    colors.forEach(color => {
-      const li = document.createElement('li')
-      li.style.backgroundColor = `rgb(${color.join(',')})`
-      ul.appendChild(li)
-    })
-    return ul
-  })
-  swatches.forEach(div => swatchesContainer.appendChild(div))
-  return swatchesContainer
 }
 
 function renderViz (ctx, screenshots, padding) {
@@ -106,42 +81,72 @@ function renderViz (ctx, screenshots, padding) {
   const center = [width / 2 | 0, height / 2 | 0]
   const maxMagnitude = (Math.min(height, width) / 2 | 0) - padding
 
+  if (settings.showColorWheel) {
+    drawColorWheel(ctx, center, maxMagnitude)
+  }
+
   if (settings.showColorConnections) {
     // draw all connections first
-    screenshots.forEach(screenshot => {
-      const colors = getColorsFromScreenshot(screenshot)
-      const screenshotCenter = getCenter(colors, maxMagnitude, center)
-      colors.forEach(color => {
-        const start = getCoordForColor(color, maxMagnitude, center)
-        drawLine(ctx, start, screenshotCenter, '#efefef')
-        drawCircle(ctx, screenshotCenter, settings.screenshotCircleSize, '#ccc')
-      })
-    })
+    drawColorConnections(ctx, center, maxMagnitude, screenshots)
   }
 
   // draw colored circles on top of the connections
+  drawColorCircles(ctx, center, maxMagnitude, screenshots)
+}
+
+function drawColorCircles (ctx, center, size, screenshots) {
   screenshots.forEach(screenshot => {
     const colors = getColorsFromScreenshot(screenshot)
     colors.forEach(color => {
-      plotColor(ctx, color, center, maxMagnitude)
+      plotColor(ctx, color, center, size)
     })
   })
 }
 
+function drawColorConnections (ctx, center, size, screenshots) {
+  screenshots.forEach(screenshot => {
+    const colors = getColorsFromScreenshot(screenshot)
+    const screenshotCenter = getCenter(colors, size, center)
+    colors.forEach(color => {
+      const start = getCoordForColor(color, size, center)
+      drawLine(ctx, start, screenshotCenter, '#efefef')
+      drawCircle(ctx, screenshotCenter, settings.screenshotCircleSize, '#ccc')
+    })
+  })
+}
+
+function drawColorWheel (ctx, center, size) {
+  const ticks = 180 * 4
+  for (let i = 0; i < ticks; i++) {
+    const percOfColorWheel = i / ticks
+    const rads = Math.PI * 2 * percOfColorWheel
+    const end = [
+      Math.cos(rads) * size + center[0],
+      Math.sin(rads) * size + center[1]
+    ]
+    const hue = 360 * percOfColorWheel | 0
+    const gradient = ctx.createLinearGradient(center[0], center[1], end[0], end[1])
+    const gradGranularity = 4
+    for (let j = 0; j < gradGranularity; j++) {
+      const lightness = (1 - (j / gradGranularity)) * 90 + 10
+      const color = Color({ h: hue, s: 100, l: lightness }).alpha(settings.ticksAlpha)
+      gradient.addColorStop(j / gradGranularity, color.string())
+    }
+    drawLine(ctx, center, end, gradient)
+
+    // draw the outside circle
+    const startRad = percOfColorWheel * Math.PI * 2
+    const endRad = (i + 1) / ticks * Math.PI * 2
+    const color = Color({ h: hue, s: 45, l: 50 }).alpha(0.8).string()
+    drawArc(ctx, center, size + 8, startRad, endRad, 3, color)
+  }
+}
+
 function getColorsFromScreenshot (screenshot) {
-  const threshold = settings.colorThreshold
+  const threshold = settings.colorThreshold / 100
   return screenshot.colors
     .filter(({ amount }) => threshold ? amount >= threshold : true)
     .map(({ color }) => color)
-}
-
-function createCanvas (width, height, padding) {
-  const canvas = document.createElement('canvas')
-  canvas.height = height
-  canvas.width = width
-  canvas.style.height = `${height}px`
-  canvas.style.width = `${width}px`
-  return canvas
 }
 
 function getCenter (colors, wheelSize, center) {
@@ -173,8 +178,16 @@ function plotColor (ctx, color, center, wheelSize) {
   const dimension = dimensions[settings.circleSizeDimension]
   const sizePerc = dimension / 100
   const scale = settings.circleSizeDimensionNegative ? (1 - sizePerc) : sizePerc
-  const circleSize = scale * settings.colorCircleMaxSize
+  const circleSize = scale * (settings.colorCircleMaxSize - 1) + 1
   drawCircle(ctx, position, circleSize, color.toString())
+}
+
+function drawArc (ctx, center, radius, startRad, endRad, width, color) {
+  ctx.strokeStyle = color
+  ctx.lineWidth = width
+  ctx.beginPath()
+  ctx.arc(center[0], center[1], radius, startRad, endRad)
+  ctx.stroke()
 }
 
 function drawCircle (ctx, position, radius, color) {
@@ -186,6 +199,7 @@ function drawCircle (ctx, position, radius, color) {
 
 function drawLine (ctx, start, end, color) {
   ctx.strokeStyle = color
+  ctx.lineWidth = 1
   ctx.beginPath()
   ctx.moveTo(start[0], start[1])
   ctx.lineTo(end[0], end[1])
