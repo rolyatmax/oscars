3c3d546e6d7f972b67a2e788c37ab328352b9645
Taylor Baldwin <taylorbaldwin@gmail.com>
2017-02-24 18:02:31 -0500

    save my place

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import lorem from 'lorem-hipsum'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App' />
+      <div className='App'>
+        <header>
+          <div className='img-container' />
+          <h1>Oscar-worthy Color Palettes</h1>
+        </header>
+        <section>
+          <p>{lorem({ count: 5 })}</p>
+          <p>{lorem({ count: 5 })}</p>
+          <p>{lorem({ count: 5 })}</p>
+          <p>{lorem({ count: 5 })}</p>
+          <p>{lorem({ count: 5 })}</p>
+        </section>
+      </div>
     )
   }
 }
